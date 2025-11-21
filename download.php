eb7fcc075b07dac89e377bc9ea042931fa526960
alexm017 <morosanua904@gmail.com>
2025-11-21 20:31:50 +0200

    Second commit, removed some things

--- download.php
+++
@@ -1,8 +0,0 @@
-<?php
-$download_option = $_GET["option"];
-if(isset($_GET["option"])){
-	if($download_option == "1"){
-		header("Location: trainingData/data/gatherTrainingData.zip");
-	}
-}
-?>
--- profile.php
+++ profile.php
@@ -8,7 +8,7 @@ session_start();
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<title>AlphaBit - OpenML</title>
+	<title>Urmele timpului</title>
 	<link rel="stylesheet" href="assets/css/style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
